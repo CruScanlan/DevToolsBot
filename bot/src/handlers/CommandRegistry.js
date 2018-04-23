8e98f2c727142687b3f05fe7ec5a333189a9d024
Cru Scanlan <cruzercru@hotmail.com>
2018-04-24 09:33:15 +1000

    Command framework design is decided. Started work on command framework.

--- bot/src/handlers/CommandRegistry.js
+++ bot/src/handlers/CommandRegistry.js
@@ -1,4 +1,5 @@
 const discord = require('discord.js');
+let fs = require('fs');
 
 class CommandRegistry {
     constructor(client) {
@@ -30,13 +31,33 @@ class CommandRegistry {
         this.commandsPath = null;
     }
 
+    register(options) {
+        if(typeof options !== 'object') throw new TypeError(`options must be an object`);
+        if(!Array.isArray(options.groups)) throw new TypeError(`options.groups must be an array`);
+        if(typeof options.commandsPath !== 'string') throw new TypeError(`options.commandsPath must be a string`);
+
+        this._registerCommandPath(options.commandsPath);
+        //this._registerCommandGroups(options.groups);
+
+    }
+
+    /**
+     * Register the commands folder file path
+     * @param path
+     * @returns {path}
+     * @private
+     */
+    _registerCommandPath(path) {
+        if(!fs.existsSync(path)) throw new Error(`${path} | file path does not exist!`);
+        this.commandsPath = path;
+        return path;
+    }
+
     _registerCommand(command) {
 
     }
 
-    registerCommandGroups(groups) {
-        if(!Array.isArray(groups)) throw new TypeError(`groups must be an array`);
-
+    _registerCommandGroups(groups) {
 
     }
 }
