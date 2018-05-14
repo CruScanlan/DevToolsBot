6bcefe47fd353118177e1db85d5bfdb26802cf9e
Cru Scanlan <cruzercru@hotmail.com>
2018-05-14 22:28:41 +1000

    Finished Bot Guild DB Syncing

--- bot/src/app.js
+++ bot/src/app.js
@@ -34,6 +34,15 @@ let insertAndUpdateGuild = (guild) => {
     })
 };
 
+let insertAndUpdateGuildManagers = (guild) => {
+    insertAndUpdateGuild(guild);
+    let adminMembers = guild.members.filter(member => {return member.hasPermission('ADMINISTRATOR') && !member.user.bot}).array();
+    if(adminMembers.length < 1) return;
+    for(let i=0; i<adminMembers.length; i++) {
+        insertAndUpdateGuildManager(guild, adminMembers[i]);
+    }
+};
+
 let insertAndUpdateGuildManager = (guild, member) => {
     let queryParams = {
         manager_id: member.id,
@@ -43,23 +52,32 @@ let insertAndUpdateGuildManager = (guild, member) => {
     })
 };
 
-let leftGuild = (guild) => {
-    let queryParams = {
-        guild_id: guild.id
-    };
-    app.coteDbRequester.send({type: "db-guilds-left", queryParams}, res => {
+let leftGuild = (guild_id) => {
+    app.coteDbRequester.send({type: "db-guilds-left", queryParams: {guild_id}}, res => {
     })
 };
 
 app.on('ready', () => {
     console.log('Bot Has Started Running!');
+
+    app.coteDbRequester.send({type: "db-guilds-get-all", queryParams: {}}, res => {
+        let dbGuildIds = res.rows.filter(row => {return row.joined === 1}).map(row => {return row.guild_id});
+        let clientGuildIds = app.guilds.map(guild => {return guild.id});
+        for(let i=0; i<dbGuildIds.length; i++) { //check what needs removing
+            if(clientGuildIds.indexOf(dbGuildIds[i]) === -1) {
+                leftGuild(dbGuildIds[i]);
+            }
+        }
+        for(let i=0; i<clientGuildIds.length; i++) { //check what needs adding
+            if(dbGuildIds.indexOf(clientGuildIds[i]) === -1) {
+                insertAndUpdateGuild(app.guilds.get(clientGuildIds[i]));
+                insertAndUpdateGuildManagers(app.guilds.get(clientGuildIds[i]));
+            }
+        }
+    })
+
 }).on('guildCreate', guild => {
-    insertAndUpdateGuild(guild);
-    let adminMembers = guild.members.filter(member => member.hasPermission('ADMINISTRATOR')).array();
-    if(adminMembers.length < 1) return;
-    for(let i=0; i<adminMembers.length; i++) {
-        insertAndUpdateGuildManager(guild, adminMembers[i]);
-    }
+    insertAndUpdateGuildManagers(guild);
 }).on('guildUpdate', (oldGuild, newGuild) => {
     insertAndUpdateGuild(newGuild);
 }).on('guildMemberAdd', guildMember => {
@@ -67,7 +85,7 @@ app.on('ready', () => {
 }).on('guildMemberRemove', guildMember => {
     insertAndUpdateGuild(guildMember.guild);
 }).on('guildDelete', guild => {
-    leftGuild(guild);
+    leftGuild(guild.id);
 });
 
 app.login(config.token);
